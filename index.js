dc18d2243f154e9ca8f85883369f358f60b6293a
szenia <s.zadvornykh@gmail.com>
2016-08-20 16:16:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// SETTINGS
+
+const LEVELS_DIR = '/levels/';
+
+// DEPENDENCIES
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
+var fs = require('fs');
+var path = require('path');
+
+// APP SETUP
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+// ROUTES
+
+app.post('/levels', function(req, res) {
+  fs.readdir(path.join(__dirname + LEVELS_DIR), function(error, files) {
+    if (error) console.log('/levels error', error);
+    else res.send(files);
+  })
+});
+
+app.post('/load', function(req, res) {
+  res.sendFile(path.join(__dirname + LEVELS_DIR + req.body.name));
+});
+
+app.post('/save', function(req, res) {
+  var data = req.body;
+  var levelName = data.level_name;
+  var levelData = data.level_data;
+  var fileName = path.join(__dirname + LEVELS_DIR + levelName + '.json');
+  
+  jsonfile.writeFile(fileName, levelData, function(error) {
+    if (error) console.log('/save error', error);
+    else res.send('level saved');
+  });
+});
+
+// RUN
+
+app.listen(3000, function() {
+  console.log('level editor running on port 3000');
+});
---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+var levels = [];
+
+function loadLevel(name) {
+  var request = new XMLHttpRequest();
+  request.open('POST', 'http://localhost:3000/level', true);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.onload = function(e) {
+    console.log('level loaded', JSON.parse(e.target.response));
+  };
+  request.send(JSON.stringify({name: name}));
+}
+
+function loadLevels() {
+  var request = new XMLHttpRequest();
+  request.open('POST', 'http://localhost:3000/levels', true);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.onload = function(e) {
+    levels = JSON.parse(e.target.response);
+    
+    console.log('levels loaded', levels);
+    
+    loadLevel(levels[0]);
+  };
+  request.send();
+}
+
+loadLevels();
+
+function save() {
+  var data = {
+    level_name: 'level_1',
+    level_data: {
+      obj_1: {
+        vertices: [
+          {x: 0, y: 0}
+        ]
+      }
+    }
+  };
+  
+  var request = new XMLHttpRequest();
+  request.open('POST', 'http://localhost:3000/save', true);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.onload = function(e) {
+    console.log('ONLOAD', e);
+  };
+  request.send(JSON.stringify(data));
+}
